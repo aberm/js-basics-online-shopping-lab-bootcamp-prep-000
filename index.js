44dfe6b30edc09006e50b270f9cd774788c81ec7
Menachem Aber <abermendel@gmail.com>
2018-11-08 22:22:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,6 +59,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === '') {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    cart = [];
     return ""
   }
 }
