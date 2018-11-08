b94e9cb2299a77edbe041e04ec1d87b731a4dee1
Menachem Aber <abermendel@gmail.com>
2018-11-08 22:29:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i]['itemName'] === item) {
-      cart = cart.splice(0, i) + cart.splice(i+1);
+      cart = cart.splice(i, 1);
       return cart
     } 
   
